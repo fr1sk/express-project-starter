951ae63727c4e792ca9f66fedfeb6e90f3a33b5a
fr1sk <fr1sk@live.com>
2018-03-31 13:57:44 +0200

    :tada: project setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send(''))
+app.get('/', (req, res) => res.send('Hello from the backend side!'));
 
 app.listen(3000, () => console.log('🎉 Running on port 3000...'))
